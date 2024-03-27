d15c212df249a106dbe71b557226ca5a9415e860
ssPLACH <134180679+ssPLACH@users.noreply.github.com>
2024-03-27 05:27:12 +0000

    funcionando, carta descentrada

--- src/app.js
+++ src/app.js
@@ -7,5 +7,54 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let switchCardButton = document.querySelector("#button");
+  switchCardButton.addEventListener("click", function() {
+    let icons = ["♦", "♥", "♠", "♣"];
+    let values = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "K",
+      "Q",
+      "J",
+      "A"
+    ];
+
+    let randomValue = Math.floor(Math.random() * values.length);
+    let randomIcons = Math.floor(Math.random() * icons.length);
+
+    function colorSuits(icon) {
+      if (icon === "♠" || icon === "♣") {
+        return "black";
+      } else {
+        return "red";
+      }
+    }
+
+    let cardHTML = `
+  <div id="card-symbol-top" class="cardSymbol ${colorSuits(
+    icons[randomIcons]
+  )}">
+  ${icons[randomIcons]}
+  </div>
+  <div class="card-number">
+    <h2>${values[randomValue]}</h2>
+  </div>
+  <div id="card-symbol-bottom" class="cardSymbol ${colorSuits(
+    icons[randomIcons]
+  )}">
+  ${icons[randomIcons]}
+  </div>
+`;
+
+    let cardContainer = document.querySelector("#card");
+    cardContainer.innerHTML = cardHTML;
+  });
 };
